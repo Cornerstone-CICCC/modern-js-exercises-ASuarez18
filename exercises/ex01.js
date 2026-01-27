ac9526992ebec7e2634e3d26ec488e9fe9a2e0c3
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-01-27 09:19:51 -0800

    exercise 1 completed

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -3,12 +3,13 @@ In this exercise, we will be given an array of 2 or more numbers.
 We will then have to find the two largest numbers in that array, and sum them together.
 
 Instruction
-Create a function named sumLargestNumbers that will receive an array of numbers and return the sum of the two largest numbers in that array.
+Create a function named sumLargestNumbers that will receive an array of numbers and return 
+the sum of the two largest numbers in that array.
 */
 
 const sumLargestNumbers = function (data) {
-  // Put your solution here
-  return 11;
+  data.sort((a, b) => a - b);
+  return data[data.length - 1] + data[data.length - 2];
 };
 
 console.log(sumLargestNumbers([1, 10])); // 11
