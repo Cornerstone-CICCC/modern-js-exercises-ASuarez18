cb2a1223d869b6e665534d8996b1ecf40d1189a8
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-01-30 10:29:57 -0800

    exercise 13 completed

--- exercises/ex13.js
+++ exercises/ex13.js
@@ -1,15 +1,24 @@
 /*
-In this activity, we will be converting date strings like "2017/12/02", into more English friendly date strings like December 2nd, 2017.
+In this activity, we will be converting date strings like "2017/12/02", into more English 
+friendly date strings like December 2nd, 2017.
 
-Talking Calendar
-We will be given a date as a string (not a Date object). The date will always be formatted as YYYY/MM/DD. We will have to parse the given string and produce a human readable date.
+> Talking Calendar
+* We will be given a date as a string (not a Date object). The date will always be formatted as YYYY/MM/DD. 
+* We will have to parse the given string and produce a human readable date.
 
-Instruction
-Create a function named talkingCalendar that takes in a date string with the format YYYY/MM/DD, and returns a new human readable date that looks like December 2nd, 2017.
+? Instruction
+Create a function named talkingCalendar that takes in a date string with the format YYYY/MM/DD, 
+and returns a new human readable date that looks like December 2nd, 2017.
 */
 
 const talkingCalendar = function (date) {
   // Your code here
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const dateSplitted = date.split('/');
+  let day = parseInt(dateSplitted[2]);
+  let month = months[parseInt(dateSplitted[1] - 1)];
+
+return `${month} ${day}${day === 1 ? "st" : day === 2 ? "nd" : day === 3 ? "rd" : "th"}, ${dateSplitted[0]}`
 };
 
 console.log(talkingCalendar("2017/12/02")); // December 2nd, 2017
