b992499f9eae5dcb5da03a8eaf826c1d4275f034
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-02-04 20:37:51 -0800

    exercise 17 completed

--- exercises/ex16.js
+++ exercises/ex16.js
@@ -1,21 +1,20 @@
-/*Caze Maker II
-We will still be given an input string to convert. However, this time, we'll also be given a casing style to work with. 
-The following code block will describe all the casing styles to support. We may also receive an array of casing styles, 
-and each of these should be applied.
+/*
+! Caze Maker II
+* We will still be given an input string to convert. However, this time, we'll also be given a casing style to work with. 
+* The following code block will describe all the casing styles to support. We may also receive an array of casing styles, 
+* and each of these should be applied.
 
-Instruction
+? Instruction
 Create a function named makeCaze that will receive an input string and one or more casing options. 
-Return a new string that is formatted based on casing options:
 
-Precedence of each of the casing styles are as follows, values higher in the list should be processed first:
-
-camel, pascal, snake, kebab, title
-vowel, consonant
-upper, lower
-Our function should be able to handle all of these cases.
+> Return a new string that is formatted based on casing options:
+> Precedence of each of the casing styles are as follows, values higher in the list should be processed first:
+- camel, pascal, snake, kebab, title
+- vowel, consonant
+- upper, lower
+- Our function should be able to handle all of these cases.
 
 For more information on casing styles, read Wikipedia's Special Case Styles for a list of various casing examples.
-
 */
 
 const makeCaze = function (input, caze) {
--- exercises/ex17.js
+++ exercises/ex17.js
@@ -1,20 +1,21 @@
-/*In this exercise, we will be given a url encoded string of key-value pairs, and we will have to turn it into a JavaScript object.
+/*
+In this exercise, we will be given a url encoded string of key-value pairs, 
+and we will have to turn it into a JavaScript object.
 
-URL Encoded Strings
-To safely send data in a URL, the data first has to be encoded to convert any special characters to URL safe characters. For this assignment we will only focus on the following URL encoding rules:
+? URL Encoded Strings
+* To safely send data in a URL, the data first has to be encoded to convert any special characters 
+* to URL safe characters. For this assignment we will only focus on the following URL encoding rules:
+- %20 represents a space character.
+- Key-value pairs are represented using an = character: key=value
+- Multiple key-value pairs are separated using a & character: key1=value1&key2=value2
+- So the following URL encoded string:
 
-%20 represents a space character.
-Key-value pairs are represented using an = character: key=value
-Multiple key-value pairs are separated using a & character: key1=value1&key2=value2
-So the following URL encoded string:
-
-city=Vancouver&weather=lots%20of%20rain
-Could be converted to the following JavaScript object:
-
-{
-  city: "Vancouver",
-  weather: "lots of rain"
-}
+`city=Vancouver&weather=lots%20of%20rain
+> Could be converted to the following JavaScript object:
+` {
+`   city: "Vancouver",
+`   weather: "lots of rain"
+` }
 
 Instruction
 Create a function named urlDecode that will receive a URL encoded string, and return the a JavaScript object that represents that data.
@@ -23,6 +24,16 @@ Create a function named urlDecode that will receive a URL encoded string, and re
 
 const urlDecode = function (text) {
   // Put your solution here
+  const spaceFormatter = (text) => text.replaceAll("%20", " ")
+
+  const split = text.split("&")
+
+  let urlObject = {};
+  for (const item of split) {
+    let itemSplitted = item.split("=")
+    urlObject[itemSplitted[0]] = spaceFormatter(itemSplitted[1]);
+  }
+  return urlObject;
 };
 
 console.log(urlDecode("duck=rubber")); //{duck: "rubber"}
