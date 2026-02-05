de914b3a0f79c96e4ecb944a7370725cc7b04df2
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-02-04 18:12:28 -0800

    exercise 12 refactor

--- exercises/ex12.js
+++ exercises/ex12.js
@@ -1,44 +1,57 @@
-  /*
-  The town festival is tomorrow and the organizers have only just realized that they've booked two bakeries 
-  to cater desserts, but only have one kitchen available. Instead of just choosing one bakery, let's help them 
-  figure out a way to work together.
+/*
+The town festival is tomorrow and the organizers have only just realized that they've booked two bakeries 
+to cater desserts, but only have one kitchen available. Instead of just choosing one bakery, let's help them 
+figure out a way to work together.
 
-  Both of the bakeries have a specialty, so they each have a stock of specific ingredients.
+Both of the bakeries have a specialty, so they each have a stock of specific ingredients.
 
-  Lucky for the festival organizers, we've found a recipe book in the town library with TONS of fusion recipes, 
-  unfortunately it's thousands of pages long and we don't have much time. Let's write a function that helps determine 
-  which recipes match the ingredients that both bakeries have in stock.
+Lucky for the festival organizers, we've found a recipe book in the town library with TONS of fusion recipes, 
+unfortunately it's thousands of pages long and we don't have much time. Let's write a function that helps determine 
+which recipes match the ingredients that both bakeries have in stock.
 
-  ? Instruction
-  We need to complete a function called chooseRecipe(), which will receive three parameters: 
-  - An array of ingredients in stock at Bakery A - An array of ingredients in stock at Bakery B 
-  - An array of recipe objects. Each recipe has a name property(string) and an 
-  - ingredient property(array)
+? Instruction
+We need to complete a function called chooseRecipe(), which will receive three parameters: 
+- An array of ingredients in stock at Bakery A - An array of ingredients in stock at Bakery B 
+- An array of recipe objects. Each recipe has a name property(string) and an 
+- ingredient property(array)
 
-  We are limiting our search to two ingredient recipes. We want to find a recipe that utilizes one ingredient 
-  from Bakery A and one from Bakery B.
+We are limiting our search to two ingredient recipes. We want to find a recipe that utilizes one ingredient 
+from Bakery A and one from Bakery B.
 
-  * Our chooseRecipe() function should return the name of the correct recipe.
+* Our chooseRecipe() function should return the name of the correct recipe.
 
-  ! Note
-  There will always be a single correct answer, and you will NOT need to consider special cases. 
-  For example, you do NOT need to worry about cases where one bakery has BOTH the ingredients for a recipe.
+! Note
+There will always be a single correct answer, and you will NOT need to consider special cases. 
+For example, you do NOT need to worry about cases where one bakery has BOTH the ingredients for a recipe.
 
-  ! Note
-  This one is a doozy! We might want to start by creating a helper function called ingredientCheck() that will 
-  take in one bakery at a time, along with the recipes.ingredients array to check if the given bakery possesses any of the 
-  ingredients from that recipe.
-  */
+! Note
+This one is a doozy! We might want to start by creating a helper function called ingredientCheck() that will 
+take in one bakery at a time, along with the recipes.ingredients array to check if the given bakery possesses any of the 
+ingredients from that recipe.
+*/
+
+const ingredientCheck = function (bakery, ingredients) {
+  let count = 0;
+  for (const ingredient of ingredients)
+    if (bakery.includes(ingredient))
+      count++;
+
+  return count;
+}
 
 const chooseRecipe = function (bakeryA, bakeryB, recipes) {
-  let name;
-  for (const recipe of recipes) { 
-    if ((bakeryA.includes(recipe.ingredients[0]) || bakeryA.includes(recipe.ingredients[1])) && (bakeryB.includes(recipe.ingredients[0]) || bakeryB.includes(recipe.ingredients[1])))
-      name = recipe.name;
+  for (const recipe of recipes) {
+    const fromA = ingredientCheck(bakeryA, recipe.ingredients);
+    const fromB = ingredientCheck(bakeryB, recipe.ingredients);
+
+    if (fromA === 1 && fromB === 1) {
+      const ingredientA = recipe.ingredients.find(ing => bakeryA.includes(ing));
+      const ingredientB = recipe.ingredients.find(ing => bakeryB.includes(ing));
+      if (ingredientA !== ingredientB)
+        return recipe.name;
+    }
   }
-
-
-  return name;
+  return null;
 };
 
 let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
